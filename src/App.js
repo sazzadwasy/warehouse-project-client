c928e7fe0d7773cfcfa38111b1fee818eb25de6b
sazzadwasy <sazzad.wasy@gmail.com>
2022-05-02 15:31:15 +0600

    carousel added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Pages/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>new assignment</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/InventoryItems/InventoryItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InventoryItems = () => {
+    return (
+        <div>
+            <p className='text-center fs-2 fw-bold'>Inventory items</p>
+        </div>
+    );
+};
+
+export default InventoryItems;

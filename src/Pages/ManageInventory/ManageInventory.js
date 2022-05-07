587fd45c3d81e5c8a0995de42fb6b0dea54d8632
sazzadwasy <sazzad.wasy@gmail.com>
2022-05-08 00:47:18 +0600

    delete inventory from db and ui

--- src/Pages/ManageInventory/ManageInventory.js
+++ src/Pages/ManageInventory/ManageInventory.js
@@ -5,6 +5,21 @@ import './ManageInventory.css'
 
 const ManageInventory = () => {
     const [inventoryes, setInventoryes] = useInventoryes()
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you want ot delete this inventory?')
+        if (proceed) {
+            const url = `http://localhost:5000/inventory/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    const rest = inventoryes.filter(inventory => inventory._id !== id)
+                    setInventoryes(rest)
+                })
+        }
+    }
     return (
         <div>
             <h1 className='text-center'>All Inventory Items</h1>
@@ -18,7 +33,7 @@ const ManageInventory = () => {
                             <p className="card-text fw-bold">Supplier : {inventory.supplier}</p>
                             <p className="card-text fw-bold">Price : {inventory.price}</p>
                             <p className="card-text fw-bold">Quantity : {inventory.quantity}</p>
-                            <button className='detail-buttons'>Delete</button>
+                            <button onClick={() => handleDelete(inventory._id)} className='detail-buttons'>Delete</button>
                         </div>
 
                     )
